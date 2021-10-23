4a9a027e688ac5584249decf34cde58372ef5cfe
nopooo <53619358+nopooo@users.noreply.github.com>
2021-10-23 19:39:54 +0200

    Zaklady MRHS

--- MRHS_Solver/MRHS.py
+++ MRHS_Solver/MRHS.py
@@ -1,5 +1,41 @@
+import random
 
 class MRHS:
 
-    def __init__(self):
-        print("Created MRHS")
+    def __init__(self,vector_size):
+        self.block_array = []
+        self.vector_size = vector_size
+
+
+
+class BlockMatrix:
+
+    def __init__(self,dim_x,dim_y,rhs):
+        self.matrix = Initialise2DMatrix(dim_x,dim_y)
+        self.rhsMatrix = rhs
+
+
+        print("Created BlockMatrix")
+
+    def PrintRow(self,row_id):
+        for row_byte in self.matrix[row_id]:
+            print(row_byte,end='')
+
+
+
+
+class RHS:
+    def __init__(self,vector_len,vector_count):
+        self.matrix = Initialise2DMatrix(vector_len,vector_count)
+
+        print("Created RHS")
+
+    def PrintRow(self,row_id):
+        for row_byte in self.matrix[row_id]:
+            print(row_byte,end='')
+
+
+def Initialise2DMatrix(dimX,dimY):
+    m =[[0] * dimX for i in range(dimY)]
+    return m
+
