6937f39aebffb1cc39f435b6927df0668c19956c
DanielJanci <DanielJanci@users.noreply.github.com>
2021-10-24 18:13:36 +0200

    Pridane SolveWithVector, FindAllSolutions a bitfield

--- MRHS_Solver/MRHS.py
+++ MRHS_Solver/MRHS.py
@@ -7,6 +7,41 @@ class MRHS:
         self.vector_size = vector_size
 
 
+    def FindAllSolutions(self):
+        solutions = []
+        num = 2 ** self.vector_size
+        print("Testing {} vectors...".format(num))
+        for i in range(num):
+            if self.SolveWithVector(i):
+                vec = bitfield(i)
+                for j in range(self.vector_size - len(vec)):
+                    vec.insert(0, 0)
+                solutions.append(vec)
+
+        return solutions
+
+
+    def SolveWithVector(self, vector):
+        vector_array = bitfield(vector)
+        vector_array_fin = vector_array.copy()
+        for i in range(self.vector_size - len(vector_array)):
+            vector_array_fin.insert(0, 0)
+        # print(vector_array_fin)
+        for block in self.block_array:
+            block_res = []
+            for row_index in range(len(block.matrix[0])):
+                num = 0
+                for col_index in range(self.vector_size):
+                    out = block.matrix[col_index][row_index] * vector_array_fin[col_index]
+                    num += out
+                block_res.append(num % 2)
+            # print(block_res)
+            if not block.rhsMatrix.contains(block_res):
+                return False
+
+        return True
+
+
 
 class BlockMatrix:
 
@@ -39,3 +74,6 @@ def Initialise2DMatrix(dimX,dimY):
     m =[[0] * dimX for i in range(dimY)]
     return m
 
+
+def bitfield(n):
+    return [int(digit) for digit in bin(n)[2:]]
