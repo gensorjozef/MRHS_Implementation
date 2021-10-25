b5bc569c25bec81094ad1b5078d5c1990bf71f88
LekyDev <jakublen80@gmail.com>
2021-10-25 14:26:00 +0200

    main function for tests, printing MRHS

--- MRHS_Solver/MRHS.py
+++ MRHS_Solver/MRHS.py
@@ -6,6 +6,31 @@ class MRHS:
         self.block_array = []
         self.vector_size = vector_size
 
+    def GenerateRandomBlockArray(self, block_count, block_size, rhs_fill, seed=1891499):
+        random.seed(seed)
+        rhs_count = int((block_size ** 2) * rhs_fill)
+        for n in range(block_count):
+            rhs = RHS(block_size, rhs_count, ran=True)
+            self.block_array.append(BlockMatrix(block_size, self.vector_size, rhs, ran=True))
+
+    def PrintMRHS(self):
+        for row in range(self.vector_size):
+            for block in self.block_array:
+                block.PrintRow(row)
+                print(' ', end='')
+            print()
+        print("-" * 30)
+        printing = True
+        rhs_id = 0
+        while (printing):
+
+            printing = False
+            for block in self.block_array:
+                if (block.TryPrintRhs(rhs_id)):
+                    printing = True
+                    print(' ', end='')
+            print()
+            rhs_id += 1
 
     def FindAllSolutions(self):
         solutions = []
@@ -45,11 +70,12 @@ class MRHS:
 
 class BlockMatrix:
 
-    def __init__(self,dim_x,dim_y,rhs):
+    def __init__(self,dim_x,dim_y,rhs,ran=False):
         self.matrix = Initialise2DMatrix(dim_x,dim_y)
         self.rhsMatrix = rhs
-
-
+        if (ran):
+            self.matrix = Fill2DMatrixRandom(self.matrix)
+            self.removeDupes()
         print("Created BlockMatrix")
 
     def removeDupes(self):
@@ -58,24 +84,59 @@ class BlockMatrix:
                 self.rhsMatrix.removeFirstDuplicate(row)
     def PrintRow(self,row_id):
         for row_byte in self.matrix[row_id]:
-            print(row_byte,end='')
+            print(row_byte, end='')
 
- def TryPrintRhs(self,rhs_id):
-        if(rhs_id < len(self.rhsMatrix.matrix)):
-            self.rhsMatrix.PrintRow(rhs_id)
-            return True
-        return False
+    def TryPrintRhs(self,rhs_id):
+            if(rhs_id < len(self.rhsMatrix.matrix)):
+                self.rhsMatrix.PrintRow(rhs_id)
+                return True
+            return False
 
 
 class RHS:
-    def __init__(self,vector_len,vector_count):
+    def __init__(self,vector_len,vector_count, ran=False):
         self.matrix = Initialise2DMatrix(vector_len,vector_count)
-
+        if(ran):
+            self.matrix = Fill2DMatrixRandom(self.matrix)
         print("Created RHS")
+
+    def containsDuplicate(self, vec):
+        count = 0
+
+        for row in self.matrix:
+            cont = True
+            for col in range(len(row)):
+                if (row[col] != vec[col]):
+                    cont = False
+            if (cont):
+                count += 1
+        if (count > 1):
+            return True
+        return False
+
+    def removeFirstDuplicate(self, vec):
+        for row in self.matrix:
+            cont = True
+            for col in range(len(row)):
+                if (row[col] != vec[col]):
+                    cont = False
+            if (cont):
+                self.matrix.remove(row)
+                return
+
+    def contains(self, vec):
+        for row in self.matrix:
+            cont = True
+            for col in range(len(row)):
+                if (row[col] != vec[col]):
+                    cont = False
+            if (cont):
+                return True
+        return False
 
     def PrintRow(self,row_id):
         for row_byte in self.matrix[row_id]:
-            print(row_byte,end='')
+            print(row_byte, end='')
 
 def Fill2DMatrixRandom(mat):
     for row in range(len(mat)):
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 from MRHS_Solver import MRHS as ms
 
 if __name__ == '__main__':
-    mrhs = ms.MRHS()
-
+    mrhs = ms.MRHS(7)
+    mrhs.GenerateRandomBlockArray(4,2,0.5,True)
+    mrhs.PrintMRHS()
+    results = mrhs.FindAllSolutions()
+    print(len(results))
+    print(results)
