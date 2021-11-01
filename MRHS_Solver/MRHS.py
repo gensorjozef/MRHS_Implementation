6120eef189d58491a8e7c39f29e14361663358e9
nopooo <53619358+nopooo@users.noreply.github.com>
2021-11-01 21:02:00 +0100

    Oprava vypisoavnia duplikatov

--- MRHS_Solver/MRHS.py
+++ MRHS_Solver/MRHS.py
@@ -29,35 +29,37 @@ class MRHS:
                 if (block.TryPrintRhs(rhs_id)):
                     printing = True
                     print(' ', end='')
+                else:
+                    print(' '*(len(block.matrix[0])+1),end='')
             print()
             rhs_id += 1
 
+    #Vytvorit generator pre kazdu solution
     def FindAllSolutions(self):
         solutions = []
         num = 2 ** self.vector_size
         print("Testing {} vectors...".format(num))
         for i in range(num):
-            if self.SolveWithVector(i):
-                vec = bitfield(i)
-                for j in range(self.vector_size - len(vec)):
-                    vec.insert(0, 0)
-                solutions.append(vec)
+            vector_array = bitfield(num)
+            vector_array_fin = vector_array.copy()
+            for i in range(self.vector_size - len(vector_array)):
+                vector_array_fin.insert(0, 0)
+            if self.SolveWithVector(vector_array_fin):
+                solutions.append(vector_array_fin)
 
         return solutions
 
 
+
     def SolveWithVector(self, vector):
-        vector_array = bitfield(vector)
-        vector_array_fin = vector_array.copy()
-        for i in range(self.vector_size - len(vector_array)):
-            vector_array_fin.insert(0, 0)
+
         # print(vector_array_fin)
         for block in self.block_array:
             block_res = []
             for row_index in range(len(block.matrix[0])):
                 num = 0
                 for col_index in range(self.vector_size):
-                    out = block.matrix[col_index][row_index] * vector_array_fin[col_index]
+                    out = block.matrix[col_index][row_index] * vector[col_index]
                     num += out
                 block_res.append(num % 2)
             # print(block_res)
