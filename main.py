53231af7a2418ad82975dbe861a262c35582de73
xkupkovic <xkupkovic@stuba.sk>
2022-03-09 11:34:25 +0100

    test: add testing code

--- main.py
+++ main.py
@@ -6,20 +6,39 @@
 from MRHS_Solver import MRHS as ms
 from MRHS_Solver import LoadFile as lf
 from MRHS_Solver import CreateFile as cf
+from MRHS_Solver.CTypes import CTypeMRHS
+from time import sleep
+
 
 if __name__ == '__main__':
 
     mat = lf.LoadFile("input.txt").get_final_matrix()
     mrhs = ms.MRHS(mat)
-    mrhs.print_mrhs()
 
-    # print(generator.__next__())
-    # print(mrhs.find_all_solutions())
-    for i in mrhs.find_solution():
-        print(i)
-    # print(mrhs.SolveWithVector(solution))
+
+    cmrhs = CTypeMRHS(mrhs=mrhs)
+    cmrhs.get_py_mrhs().print_mrhs()
+
+    cmrhs.create_mrhs_fixed(6, 4, 4, 4)
+    cmrhs.fill_mrhs_random()
+    cmrhs.get_py_mrhs().print_mrhs()
+
+    cmrhs.create_mrhs_variable(6, 4, [2, 2, 2, 3], [1, 1, 1, 2])
+    cmrhs.fill_mrhs_random()
+    cmrhs.get_py_mrhs().print_mrhs()
+
+    cmrhs.create_mrhs_variable(6, 4, [2, 2, 2, 3], [1, 1, 1, 2])
+    cmrhs.fill_mrhs_random_sparse()
+    cmrhs.get_py_mrhs().print_mrhs()
+
+    cmrhs.create_mrhs_fixed(6, 4, 4, 4)
+    cmrhs.fill_mrhs_random_sparse_extra(2)
+    cmrhs.get_py_mrhs().print_mrhs()
+
 
     file = cf.CreateFile(mrhs)
     file.create_file('output.txt')
+
+
 
 
