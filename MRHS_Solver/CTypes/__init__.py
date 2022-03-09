26507e113e6f345c87659c48cb71336d269288c2
xkupkovic <xkupkovic@stuba.sk>
2022-03-09 21:17:45 +0100

    fead: add more support of c impl

--- MRHS_Solver/CTypes/__init__.py
+++ MRHS_Solver/CTypes/__init__.py
@@ -4,27 +4,82 @@ from MRHS_Solver import MRHS, BlockMatrix
 from MRHS_Solver.CTypes.CStructs import _bm, MRHS_system
 import MRHS_Solver.CTypes.utils as utils
 import os.path
+import platform
+import time
 
-dll_name= "MRHS.dll"
+dll_name: str
+if (platform.system() == "Windows"):
+    dll_name = "MRHS.dll"
+elif (platform.system() == "Linux"):
+    dll_name = "MRHS.so"
+else:
+    raise Exception("Unsupported operating system (Supported= Windows,Linux )")
+
 dll_absolute_path = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + dll_name
 mrhs_lib = ctypes.CDLL(dll_absolute_path)
 
+# function prototypes
+mrhs_lib.wrapped_solve_rz_file_out.argtypes = (MRHS_system, c_int, POINTER(c_longlong), POINTER(c_longlong), POINTER(c_longlong),c_int,c_char_p )
+mrhs_lib.wrapped_solve_hc_file_out.argtypes = (MRHS_system, c_int, POINTER(c_longlong), POINTER(c_longlong), POINTER(c_longlong),c_int,c_char_p )
+mrhs_lib.wrapped_solve_rz.argtypes = (MRHS_system, c_int, POINTER(c_longlong), POINTER(c_longlong), POINTER(c_longlong),c_int)
+mrhs_lib.wrapped_solve_hc.argtypes = (MRHS_system, c_int, POINTER(c_longlong), POINTER(c_longlong), POINTER(c_longlong),c_int)
+
+mrhs_lib.wrapped_get_next_solution.argtype = POINTER(MRHS_system)
+
 mrhs_lib.wrapped_create_mrhs_fixed.argtypes = (c_int, c_int, c_int, c_int)
 mrhs_lib.wrapped_create_mrhs_fixed.restype = POINTER(MRHS_system)
+
 mrhs_lib.wrapped_create_mrhs_variable.argtypes = (c_int, c_int, POINTER(c_int), POINTER(c_int))
 mrhs_lib.wrapped_create_mrhs_variable.restype = POINTER(MRHS_system)
+
+
+class SolverReport:
+    def __init__(self, results: POINTER(c_longlong),
+                 run_time: float,
+                 xors: POINTER(c_longlong),
+                 solutions_count: POINTER(c_longlong),
+                 save_results: int = 0):
+        self.results: list[int] = []
+
+        save_count = 0
+        if save_results < solutions_count.contents.value:
+            save_count = save_results
+        else:
+            save_count = solutions_count.contents.value
+        if save_count > 0:
+            self.results = [results[i].value for i in range(save_count)]
+
+        self.run_time: float = run_time
+        self.xors_count: int = xors.contents.value
+        self.found_solutions: int = solutions_count.contents.value
+
+    def print_results(self):
+        print("Found solutions: {}".format(self.found_solutions))
+        print("Xors used: {}".format(self.xors_count))
+        print("Runtime (s): {}".format(self.run_time))
+
+    def print_solution(self):
+        if len(self.results) <= 0:
+            print("No solutions saved")
+            return
+
+        while True:
+            for solution in self.results:
+                print(solution)
+                yield
+
 
 class CTypeMRHS:
 
     def __init__(self, mrhs: MRHS = None):
-        self._dll_alocated = False
-        if(mrhs != None):
+        self._dll_allocated = False
+        if (mrhs != None):
             self.set_py_mrhs(mrhs)
         else:
             self._py_system = None
             self._c_system = MRHS_system()
 
-    def set_py_mrhs(self, mrhs:MRHS):
+    def set_py_mrhs(self, mrhs: MRHS):
         self._py_system = mrhs
         block_array: list[BlockMatrix] = mrhs.block_array
 
@@ -46,7 +101,7 @@ class CTypeMRHS:
         mat_blocks: _bm = self.c_system.pM
         rhs_blocks: _bm = self.c_system.pS
 
-        mat : list[list[list[int]]] = []
+        mat: list[list[list[int]]] = []
 
         for i in range(num_blocks):
             mat_block: _bm = mat_blocks[i]
@@ -57,7 +112,7 @@ class CTypeMRHS:
                 ar = utils.bitfield(mat_block.rows[j])
                 ar_len = len(ar)
                 for k in range(mat_block.ncols - ar_len):
-                    ar.insert(0,0)
+                    ar.insert(0, 0)
                 mat_block_vectors.append(ar)
 
             rhs_block_vectors = []
@@ -73,25 +128,81 @@ class CTypeMRHS:
             mat.append(comb)
         return MRHS.MRHS(mat)
 
-
-
     @property
     def c_system(self) -> MRHS_system:
         return self._c_system
 
-    def solve_hc(self,max_t: int):
-        maxt: c_int = c_int(max_t)
-        p_count = c_longlong(0)
-        pCount: POINTER(c_longlong) = pointer(p_count)
-        p_restarts = c_longlong(0)
-        pRestarts: POINTER(c_longlong) = pointer(p_restarts)
-        res = mrhs_lib.wrapped_solve_hc(self.c_system, maxt, pCount, pRestarts)
-        print(res)
+    def solve(self, max_t, report_solutions: int = 0, algorithm="hc", save_file: str = None) -> SolverReport:
+        if algorithm == "hc":
+            return self._prepare_solve(max_t, save_file, 0, report_solutions)
+        elif algorithm == "rz":
+            return self._prepare_solve(max_t, save_file, 1, report_solutions)
+        else:
+            raise Exception("Algorithm \"{}\" is not valid implementation".format(algorithm))
 
-    def create_mrhs_fixed(self,nrows: int, nblocks: int, blocksize: int, rhscount: int):
-        if (self._dll_alocated):
+    def _prepare_solve(self, max_t, save_file, algorithm_id, save_results) -> SolverReport:
+        maxt: c_int = c_int(max_t)
+
+        p_count = c_longlong(0)
+        pCount: POINTER(c_longlong) = pointer(p_count)  # Allocated
+
+        p_restarts = c_longlong(0)
+        pRestarts: POINTER(c_longlong) = pointer(p_restarts)  # Allocated
+
+        p_results: POINTER(c_longlong) = (POINTER(c_longlong))()  # Null pointer
+
+        start_time = time.time()
+        if algorithm_id == 0:
+            self.solve_hc(maxt, pCount, pRestarts, p_results, save_file, save_results)
+        elif algorithm_id == 1:
+            self.solve_rz(maxt, pCount, pRestarts, p_results, save_file, save_results)
+
+        run_time = time.time() - start_time
+        return SolverReport(p_results, run_time, pRestarts, pCount, save_results)
+
+    def get_next_solution(self):
+        return mrhs_lib.wrapped_get_next_sollution(pointer(self.c_system))
+
+    def solve_rz(self, maxt, pCount, pRestarts, p_results, save_file: str = None, save_results: int = 0):
+        res = 0
+        if save_file is None:
+            res = mrhs_lib.wrapped_solve_rz(self.c_system,
+                                            maxt,
+                                            pCount,
+                                            pRestarts,
+                                            p_results,
+                                            c_int(save_results))
+        else:
+            res = mrhs_lib.wrapped_solve_rz_file_out(self.c_system,
+                                                     maxt,
+                                                     pCount,
+                                                     pRestarts,
+                                                     p_results,
+                                                     c_int(save_results),
+                                                     create_string_buffer(("./{}".format(save_file)).encode("ASCII")))
+
+    def solve_hc(self, maxt, pCount, pRestarts, p_results, save_file: str = None, save_results: int = 0):
+        res = 0
+        if save_file is None:
+            res = mrhs_lib.wrapped_solve_hc(self.c_system,
+                                            maxt,
+                                            pCount,
+                                            pRestarts,
+                                            p_results,
+                                            c_int(save_results))
+        else:
+            res = mrhs_lib.wrapped_solve_hc_file_out(self.c_system,
+                                                     maxt,
+                                                     pCount,
+                                                     pRestarts,
+                                                     p_results,
+                                                     c_int(save_results),
+                                                     create_string_buffer(("./{}".format(save_file)).encode("ASCII")))
+
+    def create_mrhs_fixed(self, nrows: int, nblocks: int, blocksize: int, rhscount: int):
+        if self._dll_allocated:
             self._clear_mrhs()
-        self._dll_alocated = True
+        self._dll_allocated = True
 
         ret = mrhs_lib.wrapped_create_mrhs_fixed(c_int(nrows),
                                                  c_int(nblocks),
@@ -100,16 +211,16 @@ class CTypeMRHS:
 
         self._c_system = ret.contents
 
-    def create_mrhs_variable(self,nrows: int, nblocks: int, blocksizes: list[int], rhscounts: list[int]):
-        if(self._dll_alocated):
+    def create_mrhs_variable(self, nrows: int, nblocks: int, blocksizes: list[int], rhscounts: list[int]):
+        if (self._dll_allocated):
             self._clear_mrhs()
-        self._dll_alocated = True
-        blocksizes_c = (c_int *nblocks)(*blocksizes[:nblocks])
-        rhscounts_c = (c_int*nblocks)(*rhscounts[:nblocks])
-        ret =mrhs_lib.wrapped_create_mrhs_variable(c_int(nrows),
-                                                   c_int(nblocks),
-                                                   blocksizes_c,
-                                                   rhscounts_c)
+        self._dll_allocated = True
+        blocksizes_c = (c_int * nblocks)(*blocksizes[:nblocks])
+        rhscounts_c = (c_int * nblocks)(*rhscounts[:nblocks])
+        ret = mrhs_lib.wrapped_create_mrhs_variable(c_int(nrows),
+                                                    c_int(nblocks),
+                                                    blocksizes_c,
+                                                    rhscounts_c)
         self._c_system = ret.contents
 
     def fill_mrhs_random(self):
@@ -118,19 +229,14 @@ class CTypeMRHS:
     def fill_mrhs_random_sparse(self):
         mrhs_lib.wrapped_fill_mrhs_random_sparse(pointer(self.c_system))
 
-    def fill_mrhs_random_sparse_extra(self,density:int):
+    def fill_mrhs_random_sparse_extra(self, density: int):
         mrhs_lib.wrapped_fill_mrhs_random_sparse_extra(pointer(self.c_system), c_int(density))
 
     def __del__(self):
-        if(self._dll_alocated):
+        if (self._dll_allocated):
             self._clear_mrhs()
 
     def _clear_mrhs(self):
         mrhs_lib.wrapped_clear_MRHS(pointer(self.c_system))
         self._c_system = None
-        self._dll_alocated = False
-
-
-
-
-
+        self._dll_allocated = False
