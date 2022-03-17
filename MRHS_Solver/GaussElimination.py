20f0d71006bee3320e7e32145fad8d9cf6cd8931
DanielJanci <DanielJanci@users.noreply.github.com>
2022-03-17 12:34:16 +0100

    add column xor and fix row swap

--- MRHS_Solver/GaussElimination.py
+++ MRHS_Solver/GaussElimination.py
@@ -1,6 +1,19 @@
 from MRHS_Solver.CTypes import CTypeMRHS
 
 
+def create_indentity_mat(mrhs):
+    i_mat = []
+    for i in range(mrhs.vector_size):
+        row = []
+        for j in range(mrhs.vector_size):
+            if i == j:
+                row.append(1)
+            else:
+                row.append(0)
+        i_mat.append(row)
+    return i_mat
+
+
 def extract_matrix_from_mrhs(mrhs):
     mat = []
     for i in range(mrhs.vector_size):
@@ -44,52 +57,49 @@ def xor_rows(row1, row2):
     return row_out
 
 
-def swap_rows(row_index1, row_index2, mat):
-    tmp = mat[row_index1]
-    mat[row_index1] = mat[row_index2]
-    mat[row_index2] = tmp
+def swap_rows(row_id1, row_id2, mat):
+    tmp = mat[row_id1]
+    mat[row_id1] = mat[row_id2]
+    mat[row_id2] = tmp
 
 
-def get_col(mat, col_index):
+def get_col(mat, col_id):
     column = []
     for i in range(len(mat)):
-        column.append(mat[i][col_index])
+        column.append(mat[i][col_id])
     return column
 
 
-def instert_col(mat, col_index, col):
+def instert_col(mat, col_id, col):
     for i in range(len(mat)):
-        mat[i][col_index] = col[i]
+        mat[i][col_id] = col[i]
 
 
-def swap_cols(col_index1, col_ndex2, mat):
-    column1 = get_col(mat, col_index1)
-    column2 = get_col(mat, col_ndex2)
-    instert_col(mat, col_index1, column2)
-    instert_col(mat, col_ndex2, column1)
+def swap_cols(col_id1, col_id2, mat):
+    column1 = get_col(mat, col_id1)
+    column2 = get_col(mat, col_id2)
+    instert_col(mat, col_id1, column2)
+    instert_col(mat, col_id2, column1)
 
 
-def create_pivots(mat):
-    rows = len(mat)
-    cols = len(mat[0])
-    for i in range(rows):
-        for j in range(cols):
-            if mat[i][j] == 1:
-                row_i = mat[i]
-                for k in range(i + 1, rows):
-                    if mat[k][j] == 1:
-                        mat[k] = xor_rows(mat[k], row_i)
-                break
+def xor_cols(col_id1, col_id2, mat):
+    column1 = get_col(mat, col_id1)
+    column2 = get_col(mat, col_id2)
+    column_fin = []
+    for i in range(len(column1)):
+        column_fin.append(column1[i] ^ column2[i])
+    instert_col(mat, col_id2, column_fin)
 
 
-def find_row_index_of_pivot(mat, col_index):
+def find_row_id_of_pivot(mat, col_index):
     rows = len(mat)
     for i in range(rows):
         if mat[i][col_index] == 1:
             return i
+    return -1
 
 
-def find_col_index_of_pivot(mat, row_index):
+def find_col_id_of_pivot(mat, row_index):
     cols = len(mat[0])
     for j in range(cols):
         if mat[row_index][j] == 1:
@@ -97,22 +107,35 @@ def find_col_index_of_pivot(mat, row_index):
     return -1
 
 
-def is_pivot_row(row_index, col_index, mat):
-    if mat[row_index][col_index] == 0:
+def is_pivot_row(row_id, col_id, mat):
+    if mat[row_id][col_id] == 0:
         return False
-    for j in range(col_index):
-        if mat[row_index][j] == 1:
+    for j in range(col_id):
+        if mat[row_id][j] == 1:
             return False
     return True
 
 
-def is_pivot_col(row_index, col_index, mat):
-    if mat[row_index][col_index] == 0:
+def is_pivot_col(row_id, col_id, mat):
+    if mat[row_id][col_id] == 0:
         return False
-    for i in range(row_index):
-        if mat[i][col_index] == 1:
+    for i in range(row_id):
+        if mat[i][col_id] == 1:
             return False
     return True
+
+
+def create_pivots(mat):
+    rows = len(mat)
+    cols = len(mat[0])
+    for i in range(rows):
+        for j in range(cols):
+            if is_pivot_row(i, j, mat):
+                row_i = mat[i]
+                for k in range(rows):
+                    if mat[k][j] == 1 and k != i:
+                        mat[k] = xor_rows(mat[k], row_i)
+                break
 
 
 def sort_rows(mat):
@@ -122,7 +145,10 @@ def sort_rows(mat):
     while True:
         if i >= rows - 1:
             break
-        i_piv = find_row_index_of_pivot(mat, j)
+        i_piv = find_row_id_of_pivot(mat, j)
+        if i_piv == -1:
+            i += 1
+            continue
         if not is_pivot_row(i_piv, j, mat):
             j += 1
             continue
@@ -148,45 +174,77 @@ def sub_rows(mat):
             break
 
 
-def gauss_elim(mat):
+def row_elim(mat):
     create_pivots(mat)
     sort_rows(mat)
     sub_rows(mat)
 
 
 def swap_cols_blocks_rhss(mrhs):
-    last_row_index = 0
+    last_row_id = 0
     for i in range(len(mrhs.block_array)):
-        if last_row_index >= mrhs.vector_size:
+        if last_row_id >= mrhs.vector_size:
             break
         block_i = extract_block_form_mrhs(mrhs, i)
         rhs_i = extract_rhs_from_mrhs(mrhs, i)
         for j in range(len(block_i[0])):
-            if block_i[last_row_index][j] != 1:
-                new_col_index = find_col_index_of_pivot(block_i, last_row_index)
-                if new_col_index == -1:
+            if block_i[last_row_id][j] != 1:
+                new_col_id = find_col_id_of_pivot(block_i, last_row_id)
+                if new_col_id == -1:
                     break
-                swap_cols(j, new_col_index, block_i)
-                swap_cols(j, new_col_index, rhs_i)
-            last_row_index += 1
-            if last_row_index >= mrhs.vector_size:
+                swap_cols(j, new_col_id, block_i)
+                swap_cols(j, new_col_id, rhs_i)
+            last_row_id += 1
+            if last_row_id >= mrhs.vector_size:
+                break
+        update_mrhs_block(mrhs, i, block_i)
+        update_mrhs_rhs(mrhs, i, rhs_i)
+
+
+def xor_cols_blocks_rhss(mrhs):
+    last_row_id = 0
+    for i in range(len(mrhs.block_array)):
+        if last_row_id >= mrhs.vector_size:
+            break
+        block_i = extract_block_form_mrhs(mrhs, i)
+        rhs_i = extract_rhs_from_mrhs(mrhs, i)
+        for j in range(len(block_i[0])):
+            if is_pivot_col(last_row_id, j, block_i):
+                for k in range(j + 1, len(block_i[0])):
+                    if block_i[last_row_id][k] == 1:
+                        xor_cols(j, k, block_i)
+                        xor_cols(j, k, rhs_i)
+            last_row_id += 1
+            if last_row_id >= mrhs.vector_size:
                 break
         update_mrhs_block(mrhs, i, block_i)
         update_mrhs_rhs(mrhs, i, rhs_i)
 
 
 def gauss_elim_mrhs(mrhs):
+    # id_matrix = create_indentity_mat(mrhs) TODO
     matrix = extract_matrix_from_mrhs(mrhs)
-    gauss_elim(matrix)
+    row_elim(matrix)
     update_mrhs_matrix(mrhs, matrix)
     swap_cols_blocks_rhss(mrhs)
+    xor_cols_blocks_rhss(mrhs)
 
 
-# test code
+def print_mat(mat):
+    for r in mat:
+        print(*r)
+    print()
+
+
+# # test code
 # cmrhs = CTypeMRHS()
-# cmrhs.create_mrhs_fixed(6, 4, 4, 4)
-# cmrhs.fill_mrhs_random_sparse_extra(2)
+# cmrhs.create_mrhs_variable(8, 4, [6, 4, 4, 4], [4, 1, 1, 7])
+# cmrhs.fill_mrhs_random_sparse_extra(10)
+#
 # mrhs = cmrhs.get_py_mrhs()
 # mrhs.print_mrhs()
+#
 # gauss_elim_mrhs(mrhs)
 # mrhs.print_mrhs()
+#
+# print_mat(create_indentity_mat(mrhs))
