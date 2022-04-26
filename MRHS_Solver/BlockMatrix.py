951eac3d7c24b21154543c7b37988f03e9224ef5
DanielJanci <DanielJanci@users.noreply.github.com>
2022-04-26 17:06:55 +0200

    added method to create random MRHS and updated the code

--- MRHS_Solver/BlockMatrix.py
+++ MRHS_Solver/BlockMatrix.py
@@ -1,26 +1,19 @@
 from MRHS_Solver.RHS import *
+from MRHS_Solver.Utils import *
 
 
 class BlockMatrix:
     """
     Class for block in MRHS
     """
-    def _init_with_matrix(self, matrix):
+    def __init__(self, matrix):
         """
         Initialzie block matrix with input matrix
         :param matrix: input matrix
         """
         self.matrix = matrix[0]
-        self.rhsMatrix = RHS()
-        self.rhsMatrix.init_with_vectors(matrix[1])
+        self.rhsMatrix = RHS(matrix[1])
         self.pivots = 0
-
-    def __int__(self, mat):
-        """
-        Initialzie block matrix with input matrix
-        :param mat: input matrix
-        """
-        self._init_with_matrix(mat)
 
     def fill_random(self):
         """
@@ -28,14 +21,6 @@ class BlockMatrix:
         """
         fill_2d_matrix_random(self.matrix)
         self.rhsMatrix.fill_random()
-
-    def remove_block_matrix_dupes(self):
-        """
-        Removes block matrix dupes from RHS
-        """
-        for row in self.rhsMatrix.matrix:
-            if self.rhsMatrix.contains_duplicate(row):
-                self.rhsMatrix.remove_first_duplicate(row)
 
     def print_row(self, row_id):
         """
--- MRHS_Solver/Utils.py
+++ MRHS_Solver/Utils.py
@@ -40,3 +40,26 @@ def get_random_bit():
     :return: random bit (0/1)
     """
     return random.randint(0, 1)
+
+
+def generate_random_vectors(vec_len: int, num_of_vecs: int) -> list[list[int]]:
+    """
+    Creates num_of_vecs random vectors with length of vec_len.
+    :param num_of_vecs: number of vectors we want (must be lower than 2^vec_len)
+    :param vec_len: length of vectors
+    :return: vectors as a 2D list
+    """
+    vectors = []
+    vec_num = 2 ** vec_len
+    set_of_vecs = set()
+
+    while len(set_of_vecs) < num_of_vecs:
+        set_of_vecs.add(random.randint(0, vec_num - 1))
+
+    for i in set_of_vecs:
+        vec = [int(digit) for digit in bin(i)[2:]]
+        for j in range(vec_len - len(vec)):
+            vec.insert(0, 0)
+        vectors.append(vec)
+
+    return vectors
